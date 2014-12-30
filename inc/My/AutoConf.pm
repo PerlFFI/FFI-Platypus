121d821d3958a759a4ab67a4301b7dd9c4bba496
Graham Ollis <plicease@cpan.org>
2014-12-30 17:00:15 -0500

    stubs for windows that will need to be filled in

--- inc/My/AutoConf.pm
+++ inc/My/AutoConf.pm
@@ -9,18 +9,31 @@ use FindBin;
 
 my $root = $FindBin::Bin;
 
+my $prologue = <<EOF;
+#ifdef HAVE_DLFCN_H
+#include <dlfcn.h>
+#endif
+EOF
+
 sub build_configure
 {
   my($mb) = @_;
   
   my $ac = Config::AutoConf->new;
 
-  $ac->check_cc;
+  $ac->check_prog_cc;
   $ac->check_header('dlfcn.h');
+  
+  if($ac->check_decl('RTLD_LAZY', { prologue => $prologue }))
+  {
+    $ac->define_var( HAVE_RTLD_LAZY => 1 );
+  }
+  
   foreach my $lib (map { s/^-l//; $_ } split /\s+/, $Config{perllibs})
   {
     if($ac->check_lib($lib, 'dlopen'))
     {
+      $ac->define_var( 'HAVE_dlopen' => 1 );
       last;
     }
   }
--- xs/ffi_platypus.h
+++ xs/ffi_platypus.h
@@ -3,4 +3,22 @@
 
 #include "ffi_platypus_config.h"
 
+#ifndef HAVE_RTLD_LAZY
+#define RTLD_LAZY 0
+#endif
+
+#ifndef HAVE_dlopen
+
+void *ffi_platypus_dlopen(const char *filename, int flag);
+char *ffi_platypus_dlerror(void);
+void *ffi_platypus_dlsym(void *handle, const char *symbol);
+int ffi_platypus_dlclose(void *handle);
+
+#define dlopen(filename, flag) ffi_platypus_dlopen(filename, flag)
+#define dlerror() ffi_platypus_dlerror()
+#define dlsym(handle, symbol) ffi_platypus_dlsym(handle, symbol)
+#define dlclose(handle) ffi_platypus_dlclose(handle)
+
+#endif
+
 #endif
