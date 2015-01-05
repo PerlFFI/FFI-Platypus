664e8cd1a68bec5f7ad8cf3f9c25ade5581f4f6d
Graham Ollis <plicease@cpan.org>
2015-01-05 16:09:41 -0500

    doco

--- lib/FFI/Platypus/Declare.pm
+++ lib/FFI/Platypus/Declare.pm
@@ -5,7 +5,29 @@ use warnings;
 use FFI::Platypus;
 use base qw( Exporter );
 
+# ABSTRACT: Declarative interface to FFI::Platypus
+# VERSION
+
 our @EXPORT = qw( ffi lib type function );
+
+=head1 SYNOPSIS
+
+ use FFI::CheckLib;
+ use FFI::Platypus::Declare;
+ use constant u8 => 'u8';
+ use constant string => 'string';
+ 
+ lib find_lib lib => 'mylib', symbol => 'my_function';
+ type 'uint8' => 'u8';
+ function my_function => [ u8 ] => 'string';
+
+=head1 DESCRIPTION
+
+This module provides a declarative interface to L<FFI::Platypus>.
+It provides a more concise interface at the cost of a little less
+power, and a little more namespace pollution.
+
+=cut
 
 our $ffi;
 
@@ -15,6 +37,12 @@ sub _ffi_object
   $ffi->{$caller} ||= FFI::Platypus->new;
 }
 
+=head1 FUNCTIONS
+
+=head2 ffi
+
+=cut
+
 sub ffi (&)
 {
   my $caller = caller;
@@ -22,15 +50,43 @@ sub ffi (&)
   $_[0]->();
 }
 
+=head2 lib
+
+ lib '/lib/libfoo.so';
+
+Specify one or more dynamic libraries to search for symbols.
+If you are unsure of the location / version of the library then
+you can use L<FFI::CheckLib#find_lib>.
+
+=cut
+
 sub lib (@)
 {
   _ffi_object->lib(@_);
 }
 
+=head2 type
+
+ type 'uint8' => 'my_unsigned_int_8';
+
+Declare the given type.
+
+=cut
+
 sub type ($;$)
 {
   _ffi_object->type(@_);
 }
+
+=head2 function
+
+ function 'my_function', ['uint8'] => 'string';
+ function ['my_c_function_name' => 'my_perl_function_name'], ['uint8'] => 'string';
+
+Attach the given function with argument and return types.  You can use a two element
+array reference to install the function with a different name in perl space.
+
+=cut
 
 sub function ($$$)
 {
