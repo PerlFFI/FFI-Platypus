4abd6096bd21cad1c28808228fbfea0ffb0dea12
Graham Ollis <plicease@cpan.org>
2015-01-07 17:28:48 -0500

    start over

--- example/puts-platypus.pl
+++
@@ -1,9 +0,0 @@
-# FFI::Platypus = libffi bindings for Perl
-# written in XS
-use strict;
-use warnings;
-use v5.10;
-use FFI::Platypus;
-
-ffi_sub [], puts => [ ffi_type c => 'int', ffi_type ffi => 'string' ];
-puts("hi there");
