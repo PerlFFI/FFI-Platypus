fb6c6d968b397f024cb8d149980dd60acc670fa1
Graham Ollis <plicease@cpan.org>
2015-01-14 16:39:36 -0500

    if no sub ref is provided then pass through

--- xs/custom.c
+++ xs/custom.c
@@ -8,29 +8,36 @@
 SV*
 ffi_pl_custom_perl(SV *subref, SV *in_arg)
 {
-  dSP;
-
-  int count;
-  SV *out_arg;
-
-  ENTER;
-  SAVETMPS;
-  PUSHMARK(SP);
-  XPUSHs(in_arg);
-  PUTBACK;
-
-  count = call_sv(subref, G_SCALAR);
-
-  SPAGAIN;
-
-  if(count == 0)
-    out_arg = NULL;
+  if(subref == NULL)
+  {
+    return newSVsv(in_arg);
+  }
   else
-    out_arg = SvREFCNT_inc(POPs);
+  {
+    dSP;
 
-  PUTBACK;
-  FREETMPS;
-  LEAVE;
+    int count;
+    SV *out_arg;
 
-  return out_arg;
+    ENTER;
+    SAVETMPS;
+    PUSHMARK(SP);
+    XPUSHs(in_arg);
+    PUTBACK;
+
+    count = call_sv(subref, G_SCALAR);
+
+    SPAGAIN;
+
+    if(count == 0)
+      out_arg = NULL;
+    else
+      out_arg = SvREFCNT_inc(POPs);
+
+    PUTBACK;
+    FREETMPS;
+    LEAVE;
+
+    return out_arg;
+  }
 }
