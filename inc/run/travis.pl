99115470a9ec964ece790cd84665d3d1d233072e
Graham Ollis <perl@wdlabs.com>
2015-01-25 03:24:37 -0500

    travis add version in order to test reverse deps, we need to add the version numbers to the .pm files, so that the CPAN version of FFI::Platypus won't overwrite our version.

---
+++ inc/run/travis.pl
@@ -0,0 +1,39 @@
+use strict;
+use warnings;
+use File::Find;
+use JSON::PP qw( encode_json decode_json );
+
+my @list;
+
+# worlds worst interface
+find(sub {
+
+  my $fn = $File::Find::name;
+  return unless $fn =~ /\.pm/;
+  push @list, $fn;
+
+}, 'lib');
+
+die "requires MYMETA.json" unless -f 'MYMETA.json';
+
+my %meta = do {
+  open my $fh, '<', 'MYMETA.json';
+  my $meta = decode_json do { local $/; <$fh> };
+  close $fh;
+  %$meta;
+};
+
+foreach my $fn (@list)
+{
+  open my $in, '<', $fn;
+  my @list = <$in>;
+  close $in;
+  
+  @list = map { s/^(# VERSION.*)$/our \$VERSION = '$meta{version}'; $1/; $_ } @list;
+
+  $DB::single = 1;
+  
+  open my $out, '>', $fn;
+  print $out @list;
+  close $out;
+}
