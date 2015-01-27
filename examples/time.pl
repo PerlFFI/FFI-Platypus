1b50a9cc0ed07d02929027a74381dccd2633a8b6
Graham Ollis <plicease@cpan.org>
2015-01-27 17:57:34 -0500

    record examples in the Type.pod (#21,#22)

--- examples/time.pl
+++ examples/time.pl
@@ -2,12 +2,17 @@ use strict;
 use warnings;
 use Convert::Binary::C;
 use FFI::Platypus;
-use YAML qw( Dump );
+use Data::Dumper qw( Dumper );
 
 my $c = Convert::Binary::C->new;
+
 # Alignment of zero (0) means use
 # the alignment of your CPU
 $c->configure( Alignment => 0 );
+
+# parse the tm record structure so
+# that Convert::Binary::C knows
+# what to spit out and suck in
 $c->parse(<<ENDC);
 struct tm {
   int tm_sec;
@@ -24,8 +29,12 @@ struct tm {
 };
 ENDC
 
+# get the size of tm so that we can give it
+# to Platypus
 my $tm_size = $c->sizeof("tm");
 
+# create the Platypus instance and create the appropriate
+# types and functions
 my $ffi = FFI::Platypus->new;
 $ffi->lib(undef);
 $ffi->type("record($tm_size)" => 'tm');
@@ -34,13 +43,18 @@ $ffi->attach( [ time      => 'my_time'      ] => ['tm']      => 'time_t' );
 
 # ===============================================
 # get the tm struct from the C localtime function
+# note that we pass in a reference to the value that time
+# returns because localtime takes a pointer to time_t
+# for some reason.
 my $time_hashref = $c->unpack( tm => my_localtime(\time) );
 
 # tm_zone comes back from Convert::Binary::C as an opaque,
-# cast it into a string:
+# cast it into a string.  We localize it to just this do
+# block so that it will be a pointer when we pass it back
+# to C land below.
 do {
   local $time_hashref->{tm_zone} = $ffi->cast(opaque => string => $time_hashref->{tm_zone});
-  print YAML::Dump($time_hashref);
+  print Dumper($time_hashref);
 };
 
 # ===============================================
