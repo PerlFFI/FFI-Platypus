6efb1959712097e553853410e4103efbaf23943c
Graham Ollis <perl@wdlabs.com>
2015-01-28 21:20:16 -0500

    rust examples are moving to FFI::Platypus::Lang::Rust

--- examples/rust/string.pl
+++
@@ -1,8 +0,0 @@
-use strict;
-use warnings;
-use FFI::Platypus::Declare qw( string );
-
-lib './libstring.so';
-attach hello_rust => [] => 'string';
-
-print hello_rust();
--- examples/rust/string.rs
+++
@@ -1,9 +0,0 @@
-// compile with: rustc --crate-type dylib string.rs
-// borrowed from:
-// http://doc.rust-lang.org/book/ffi.html
-
-
-#[no_mangle]
-pub extern "C" fn hello_rust() -> *const u8 {
-  "Hello, world\n\0".as_ptr()
-}
