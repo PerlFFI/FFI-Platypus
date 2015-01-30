8f7e75075e268883543729563d2a7a443c7d4240
Graham Ollis <perl@wdlabs.com>
2015-01-30 15:21:40 -0500

    for the future

--- lib/FFI/Platypus/Lang/ASM.pm
+++ lib/FFI/Platypus/Lang/ASM.pm
@@ -16,6 +16,9 @@ use warnings;
 
 Setting your lang to C<ASM> removes all non ffi types.  
 
+This document will one day include information on bundling Assembly
+with your Perl / FFI / Platypus distribution.  Pull requests welcome!
+
 =head1 METHODS
 
 =head2 native_type_map
