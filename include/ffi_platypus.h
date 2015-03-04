acfc837d8b8a1c8febfed123fd04677c196ad315
Graham Ollis <plicease@cpan.org>
2015-03-04 10:13:39 -0500

    _malloc may allocate on the heap

--- include/ffi_platypus.h
+++ include/ffi_platypus.h
@@ -222,7 +222,7 @@ typedef struct _ffi_pl_record_member {
 #define ffi_pl_arguments_pointers(arguments) ((void**)&arguments->slot[arguments->count])
 
 #if defined(_MSC_VER)
-#define Newx_or_alloca(ptr, count, type) ptr = _malloca(sizeof(type)*count)
+#define Newx_or_alloca(ptr, count, type) ptr = _alloca(sizeof(type)*count)
 #define Safefree_or_alloca(ptr) 
 #define HAVE_ALLOCA 1
 #elif defined(HAVE_ALLOCA)
