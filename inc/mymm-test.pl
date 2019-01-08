20a5665108f46b3c2f195b1b6e3784e57f6f4b1f
Graham Ollis <plicease@cpan.org>
2019-01-08 12:53:05 -0500

    do not use once from test

--- inc/mymm-test.pl
+++ inc/mymm-test.pl
@@ -1,11 +1,8 @@
 use strict;
 use warnings;
 use lib 'inc';
-use My::Once;
 use My::LibTest;
 use My::ShareConfig;
-
-#My::Once->check('test');
 
 my $share_config = My::ShareConfig->new;
 My::LibTest->build(
@@ -14,5 +11,3 @@ My::LibTest->build(
   [],
   $share_config->get('extra_linker_flags'),
 );
-
-#My::Once->done;
