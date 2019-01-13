6c7f06d5b3b2b903c5f5dc7bb7475b3df439519d
Graham Ollis <plicease@cpan.org>
2019-01-13 08:31:32 -0500

    ffi-probe: missing file

---
+++ inc/Alien/psapi.pm
@@ -0,0 +1,32 @@
+package Alien::psapi;
+
+use strict;
+use warnings;
+use Config;
+
+sub cflags {''}
+
+sub libs
+{
+  if($^O eq 'MSWin32')
+  {
+    if($Config{ccname} eq 'cl')
+    {
+      return "psapi.lib ";
+    }
+    else
+    {
+      return "-lpsapi";
+    }
+  }
+  elsif($^O eq 'cygwin' || $^O eq 'msys')
+  {
+    return "-L/usr/lib/w32api -lpsapi ";
+  }
+  '';
+}
+
+sub install_type {'system'}
+
+1;
+
