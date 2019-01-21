e7c46aa5e25667d03691a79f649374405c4717c1
Graham Ollis <plicease@cpan.org>
2019-01-20 20:18:24 -0500

    handle this with a diagnostic

--- inc/My/Probe.pm
+++ inc/My/Probe.pm
@@ -227,7 +227,15 @@ sub configure
           },
         );
       }
-      %abi = %{ $probe->data->{abi} || {} };
+      if(defined $probe->data->{abi})
+      {
+        %abi = %{ $probe->data->{abi} || {} };
+      }
+      else
+      {
+        print "Unable to verify any ffi_abis.\n";
+        print "only default ABI will be available\n";
+      }
     }
     else
     {
