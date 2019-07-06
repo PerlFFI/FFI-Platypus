98efbc3d26ea367793e773d4127ebf60005977ce
Graham Ollis <plicease@cpan.org>
2019-07-06 12:24:23 -0400

    oops forgot variadic test function

---
+++ t/ffi/variadic.c
@@ -0,0 +1,23 @@
+#include <ffi_platypus.h>
+#ifdef FFI_PL_PROBE_VARIADIC
+#include <stdio.h>
+#include <stdarg.h>
+
+int
+variadic_return_arg(int which, ...)
+{
+  va_list args;
+  va_start(args, which);
+  int i, val;
+
+  for(i=0; i<which; i++)
+  {
+    val = va_arg(args, int);
+  }
+
+  va_end(args);
+
+  return val;
+}
+
+#endif
