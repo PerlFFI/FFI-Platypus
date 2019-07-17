7cbcf571dc3a6b939ee7cbc726ba73b7ea77087a
Graham Ollis <plicease@cpan.org>
2019-07-17 17:28:57 -0400

    oops forgot to terminate that with a NULL

--- inc/probe/recordvalue.c
+++ inc/probe/recordvalue.c
@@ -25,27 +25,32 @@ dlmain(int argc, char *argv[])
   ffi_type ffi_type_foo_t;
   int i;
   foo_t foo;
+  ffi_type *arg_types[1] = { &ffi_type_void };
+  void *args[1] = { NULL };
 
   ffi_type_foo_t.size = ffi_type_foo_t.alignment = 0;
   ffi_type_foo_t.type = FFI_TYPE_STRUCT;
-  ffi_type_foo_t.elements = calloc(14, sizeof(ffi_type*));
+  ffi_type_foo_t.elements = calloc(15, sizeof(ffi_type*));
 
   for(i=0; i<13; i++)
     ffi_type_foo_t.elements[i] = is_signed(char) ? &ffi_type_sint8 : &ffi_type_uint8;
   ffi_type_foo_t.elements[13] = &ffi_type_sint32;
+  ffi_type_foo_t.elements[14] = NULL;
 
-  if(ffi_prep_cif(&cif, FFI_DEFAULT_ABI, 0, &ffi_type_foo_t, NULL) == FFI_OK)
+  if(ffi_prep_cif(&cif, FFI_DEFAULT_ABI, 0, &ffi_type_foo_t, arg_types) == FFI_OK)
   {
-    ffi_call(&cif, (void*) get_foo, &foo, NULL);
+
+    ffi_call(&cif, (void*) get_foo, &foo, args);
+
     if(strcmp(foo.name, "hello"))
       return 2;
+
     if(foo.value != 42)
       return 2;
+
   }
   else
-  {
     return 2;
-  }
 
   return 0;
 }
