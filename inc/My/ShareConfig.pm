e8d3369f4fa7e7cdc5ac54315a22b8afa5c92090
Graham Ollis <plicease@cpan.org>
2019-07-29 16:52:43 -0400

    make shareconfig subclassable

--- inc/My/ShareConfig.pm
+++ inc/My/ShareConfig.pm
@@ -2,53 +2,9 @@ package My::ShareConfig;
 
 use strict;
 use warnings;
-use Data::Dumper ();
-use File::Spec;
-use File::Path qw( mkpath );
+use base qw( My::ConfigPl );
 
-my $dir  = File::Spec->catdir( qw( blib lib auto share dist FFI-Platypus ));
-my $file = File::Spec->catfile( $dir, qw( config.pl ));
-
-sub new
-{
-  my $data;
-  if(-e $file)
-  {
-    $data = do "./$file";
-  }
-  else
-  {
-    $data = { 'test-key' => 'test-value' };
-  }
-  bless { data => $data }, __PACKAGE__;
-}
-
-sub get
-{
-  my($self, $name) = @_;
-  $self->{data}->{$name};
-}
-
-sub set
-{
-  my($self, $name, $value) = @_;
-  $self->{data}->{$name} = $value;
-
-  my $dd = Data::Dumper->new([$self->{data}],['x'])
-    ->Indent(1)
-    ->Terse(0)
-    ->Purity(1)
-    ->Sortkeys(1)
-    ->Dump;
-
-  mkpath( $dir, 0, 0755 ) unless -d $dir;
-
-  my $fh;
-  open($fh, '>', $file) || die "error writing $file";
-  print $fh 'do { my ';
-  print $fh $dd;
-  print $fh '$x;}';
-  close $fh;
-}
+sub dir  { File::Spec->catdir( qw( blib lib auto share dist FFI-Platypus )) }
+sub file { File::Spec->catfile( shift->dir, qw( config.pl ))                }
 
 1;
