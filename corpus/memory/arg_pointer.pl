e3fb99b5d9c076cc29d72e4db668022aeefa24bf
Graham Ollis <plicease@cpan.org>
2019-10-11 16:09:46 -0600

    regression leak tests for all argument types

---
+++ corpus/memory/arg_pointer.pl
@@ -0,0 +1,59 @@
+use strict;
+use warnings;
+use FFI::Platypus;
+use Test::More;
+use Math::Complex;
+use Test::LeakTrace qw( no_leaks_ok );
+
+my @types = map { "$_*" } ( 'float', 'double', 'longdouble',
+            map { ( "sint$_" , "uint$_" ) }
+            qw( 8 16 32 64 ));
+
+foreach my $type (@types)
+{
+  subtest $type => sub {
+    my $ffi = FFI::Platypus->new;
+    my $f = $ffi->function(0 => [ $type ] => 'void' );
+    no_leaks_ok { $f->call(\129) };
+    no_leaks_ok { $f->call(undef) };
+  }
+}
+
+subtest 'opaque' => sub {
+  my $ffi    = FFI::Platypus->new( lib => [undef] );
+  my $malloc = $ffi->function( malloc => [ 'size_t' ] => 'opaque' );
+  my $free   = $ffi->function( free => [ 'opaque' ] => 'void' );
+  my $ptr    = $malloc->call(200);
+  my $f      = $ffi->function(0 => [ 'opaque*' ] => 'void' );
+
+  no_leaks_ok { $f->call(\$ptr) };
+  $free->call($ptr);
+  no_leaks_ok { $f->call(undef) };
+};
+
+subtest 'string' => sub {
+  my $ffi = FFI::Platypus->new;
+  my $f = $ffi->function(0 => [ 'string*' ] => 'void' );
+  no_leaks_ok { $f->call(\"hello world") };
+  my $str = "hello world";
+  no_leaks_ok { $f->call(\$str) };
+  no_leaks_ok { $f->call(undef) };
+};
+
+subtest 'complex' => sub {
+
+  foreach my $type (qw( complex_float* complex_double* ))
+  {
+    subtest $type => sub {
+      my $ffi = FFI::Platypus->new;
+      my $f = $ffi->function(0 => [ $type ] => 'void' );
+      my $c = Math::Complex->make(1.0,2.0);
+      no_leaks_ok { $f->call(\[1.0,2.0]) };
+      no_leaks_ok { $f->call(\$c) };
+      no_leaks_ok { $f->call(undef) };
+    };
+  }
+
+};
+
+done_testing;
