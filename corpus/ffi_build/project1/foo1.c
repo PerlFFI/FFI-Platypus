76a003583c11ac5439cf0a33c64b1f3fbfa0013b
Graham Ollis <plicease@cpan.org>
2019-12-12 16:37:19 -0700

    Some additional VC++ fixes

--- corpus/ffi_build/project1/foo1.c
+++ corpus/ffi_build/project1/foo1.c
@@ -1,3 +1,10 @@
+#ifdef _MSC_VER
+#define EXPORT __declspec(dllexport)
+#else
+#define EXPORT
+#endif
+
+EXPORT
 int
 foo1()
 {
--- corpus/ffi_build_mm/project1/ffi/x.c
+++ corpus/ffi_build_mm/project1/ffi/x.c
@@ -1,4 +1,12 @@
-int frooble_runtime()
+#ifdef _MSC_VER
+#define EXPORT __declspec(dllexport)
+#else
+#define EXPORT
+#endif
+
+EXPORT
+int
+frooble_runtime()
 {
   return 47;
 }
