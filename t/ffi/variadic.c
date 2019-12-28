77c9917a3db756818a756c55754bed44958c2b84
Graham Ollis <plicease@cpan.org>
2019-12-28 07:38:17 -0700

    add test that replicates example for gh#210

--- t/ffi/variadic.c
+++ t/ffi/variadic.c
@@ -7,18 +7,67 @@
 EXTERN int
 variadic_return_arg(int which, ...)
 {
-  va_list args;
-  va_start(args, which);
+  va_list ap;
   int i, val;
+
+  va_start(ap, which);
 
   for(i=0; i<which; i++)
   {
-    val = va_arg(args, int);
+    val = va_arg(ap, int);
   }
 
-  va_end(args);
+  va_end(ap);
 
   return val;
+}
+
+EXTERN const char *
+xprintf(const char *fmt, ...)
+{
+  va_list ap;
+  static char buffer[2046];
+  char *bp=buffer;
+
+  va_start(ap, fmt);
+
+  while(*fmt != '\0')
+  {
+    switch(*fmt)
+    {
+      case '%':
+        {
+          char buffer2[64];
+          const char *str=buffer2;
+          switch(*(++fmt))
+          {
+            case 'd':
+              sprintf(buffer2, "%d", va_arg(ap, int));
+              break;
+            case 's':
+              str = va_arg(ap, char *);
+              break;
+            default:
+              str = "[fmt error]";
+              break;
+          }
+          strcpy(bp, str);
+          bp += strlen(str);
+        }
+        break;
+
+      default:
+        *(bp++) = *fmt;
+        break;
+    }
+    fmt++;
+  }
+
+  va_end(ap);
+
+  *bp = '\0';
+
+  return buffer;
 }
 
 #endif
