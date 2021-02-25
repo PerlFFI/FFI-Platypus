13398dd1e068a52a2185484432c43e430cce798d
Graham Ollis <plicease@cpan.org>
2021-02-25 13:05:52 -0700

    bump readme + ABW

--- inc/Alien/Base/Wrapper.pm
+++ inc/Alien/Base/Wrapper.pm
@@ -14,7 +14,7 @@ use Text::ParseWords qw( shellwords );
 # for this [AlienBase::Wrapper::Bundle]
 
 # ABSTRACT: Compiler and linker wrapper for Alien
-our $VERSION = '2.33'; # VERSION
+our $VERSION = '2.38'; # VERSION
 
 
 sub _join
@@ -317,7 +317,7 @@ Alien::Base::Wrapper - Compiler and linker wrapper for Alien
 
 =head1 VERSION
 
-version 2.33
+version 2.38
 
 =head1 SYNOPSIS
 
