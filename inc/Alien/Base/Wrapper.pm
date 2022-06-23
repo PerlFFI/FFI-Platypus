3aa0b8cfa5b85bb8be69583ec0ed7d9924138600
Graham Ollis <plicease@cpan.org>
2022-06-23 15:30:03 -0500

    bump ABW to 2.50

--- inc/Alien/Base/Wrapper.pm
+++ inc/Alien/Base/Wrapper.pm
@@ -14,7 +14,7 @@ use Text::ParseWords qw( shellwords );
 # for this [AlienBase::Wrapper::Bundle]
 
 # ABSTRACT: Compiler and linker wrapper for Alien
-our $VERSION = '2.49'; # VERSION
+our $VERSION = '2.50'; # VERSION
 
 
 sub _join
@@ -325,7 +325,7 @@ Alien::Base::Wrapper - Compiler and linker wrapper for Alien
 
 =head1 VERSION
 
-version 2.49
+version 2.50
 
 =head1 SYNOPSIS
 
